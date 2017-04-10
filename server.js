530b9417286e67f71d385ec069d1f565d221a0bf
Laura Maillet <50ftwomen@gmail.com>
2017-04-10 08:57:47 -0700

    changes to server.js

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
-var express    = require('express'),
-    app        = express(),
-    mongoose   = require('mongoose'),
-    morgan     = require('morgan'),
-    bodyParser = require('body-parser')
+var express      = require('express'),
+    app          = express(),
+    mongoose     = require('mongoose'),
+    passport     = require('passport'),
+    flash        = require('connect-flash'),
+    ejsLayouts   = require('express-ejs-layouts'),
+    path         = require('path'),
+    morgan       = require('morgan'),
+    bodyParser   = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    session      = require('express-session')
+
+require('dotenv').config();
 
 var db = process.env.MONGODB_URL || 'mongodb://localhost/unleashed';
 mongoose.connect(db);
 
 app.use(morgan('dev'));
+app.use(cookieParser());
 app.use(bodyParser());
+
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.set('views', './views');
+app.use(express.status(path.join(__dirname, public)));
+
+app.use(session({ secret: 'unleashed', cookie: {maxAge: 14400000} }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./config/passport')(passport);
+
+app.use(function (req, res, next) {
+  global.user = req.user;
+  next();
+})
 
 var routes = require('./config/routes');
 app.use('/', routes);
